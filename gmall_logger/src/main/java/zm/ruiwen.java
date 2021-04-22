9b9a255307c7bad0c891597c4a8f0048b35db32c
zm008 <992683374@qq.com>
2021-04-22 22:08:49 +0800

    新分支瑞雯

---
+++ gmall_logger/src/main/java/zm/ruiwen.java
@@ -0,0 +1,12 @@
+package zm;
+
+/**
+ * @description:
+ * @author: ManolinCoder
+ * @time: 2021/4/22
+ */
+public class ruiwen {
+    public static void main(String[] args) {
+        System.out.println("ruiwen");
+    }
+}
