b2a08a0471a5262f640264fc3422507eb4473ccb
zm008 <992683374@qq.com>
2021-04-22 20:49:51 +0800

    修改111111

--- gmall_logger/src/main/java/com/Test1.java
+++ gmall_logger/src/main/java/com/Test1.java
@@ -9,5 +9,6 @@ public class Test1 {
     public static void main(String[] args) {
         System.out.println("commmmmmmmmm");
         System.out.println("zm008的提交");
+        System.out.println("11111111111");
     }
 }
